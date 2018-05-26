aa6afffb4c84cd787de6b6492ceae264fd8aee7b
Arnab Chakravarty <arnab.chak96@gmail.com>
2018-05-26 21:58:23 +0530

    Add files via upload

---
+++ min_dis_in_infinte_grid.c
@@ -0,0 +1,9 @@
+int Solution::coverPoints(vector<int> &A, vector<int> &B) {
+    int i ,res=0;
+    for(i=0;i<A.size()-1;i++)
+    {
+        res += max( abs(A[i] - A[i+1]) , abs(B[i] - B[i+1]) );
+    }
+
+    return res;
+}
